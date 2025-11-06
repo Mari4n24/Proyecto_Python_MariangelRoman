440aab44bba2864f217b1826d37a4fcf9bcdd5bb
Mari <marur00911@gmail.com>
2025-11-06 10:13:24 -0300

    Agregar configuración de directorios de plantillas y crear archivo 'inicio.html'

--- seguimiento/settings.py
+++ seguimiento/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'seguimiento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': ['ubicacion1', 'ubicacion2', 'ubicacion3'],
+        'DIRS': [r'C:\proyecto_python\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
