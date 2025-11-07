37ba1162428e1fc4626d3f9d3e66d925dca9514d
Mari <marur00911@gmail.com>
2025-11-07 16:28:13 -0300

    Actualizar modelo 'Colectividad' para incluir método '__str__' y modificar plantilla 'listar_colectividades' para mejorar la presentación.

--- inicio/models.py
+++ inicio/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 class Colectividad(models.Model):
     nombre = models.CharField(max_length=100) 
-    pais = models.CharField(max_length=100)    
+    pais = models.CharField(max_length=100)  
+    
+    def __str__(self):
+        return f'Colectividad ({self.id}): {self.nombre} - {self.pais}'
